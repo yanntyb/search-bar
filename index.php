f2f7eb7caf2d29926865f5226b3504513740d385
yann tyb <yanntyb.lbc@gmail.com>
2021-07-28 17:09:16 +0200

    liste_francais.txt

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+        <title>Woordle</title>
     <link rel="stylesheet" href="CSS/main.css">
     <script src="https://kit.fontawesome.com/78e483bd6f.js" crossorigin="anonymous"></script>
 </head>
